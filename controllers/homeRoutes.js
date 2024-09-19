ef15ac8133271ba477b4b75d650ae62007eb2bdb
IWilboI <mr.wrc1998@gmail.com>
2024-09-18 21:00:53 -0600

    routes/api

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,52 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
+
+// Render homepage with all posts
+router.get('/', async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [{ model: User, attributes: ['username'] }],
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+    res.render('homepage', { posts, logged_in: req.session.logged_in });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Render individual post page with comments
+router.get('/post/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        { model: User, attributes: ['username'] },
+        {
+          model: Comment,
+          include: [User],
+        },
+      ],
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id' });
+      return;
+    }
+
+    const post = postData.get({ plain: true });
+    res.render('post', { post, logged_in: req.session.logged_in });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Render login/signup page
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login');
+});
+
+module.exports = router;
